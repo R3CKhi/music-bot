1ff4a2fa9c4d410fbfef37cc6697fc4d7ead15cb
Glitch (comfortable-receptive-cellar) <none>
2023-02-12 00:39:48 +0300

    v1

--- config/bot.js
+++ config/bot.js
@@ -1,17 +1,17 @@
 module.exports = {
-    emojis: {
-        off: ':x:',
-        error: ':warning:',
-        queue: ':bar_chart:',
-        music: ':musical_note:',
-        success: ':white_check_mark:',
-    },
+  emojis: {
+    off: ':x:',
+    error: ':warning:',
+    queue: ':bar_chart:',
+    music: ':musical_note:',
+    success: ':white_check_mark:',
+  },
 
-    discord: {
-        token: 'ODUwMTA3MjYzNTMxMzUyMDc0.YLk6FQ.j2hfZzeNIslQwzR7SRQOvviaaeo',
-        prefix: ':',
-        activity: 'Country Music',
-    },
+  discord: {
+    token: '.G3OGF-.W74WSXT8-',
+    prefix: 'q',
+    activity: 'Being a good girl',
+  },
 
-    filters: ['8D', 'gate', 'haas', 'phaser', 'treble', 'tremolo', 'vibrato', 'reverse', 'karaoke', 'flanger', 'mcompand', 'pulsator', 'subboost', 'bassboost', 'vaporwave', 'nightcore', 'normalizer', 'surrounding'],
+  filters: ['8D', 'gate', 'haas', 'phaser', 'treble', 'tremolo', 'vibrato', 'reverse', 'karaoke', 'flanger', 'mcompand', 'pulsator', 'subboost', 'bassboost', 'vaporwave', 'nightcore', 'normalizer', 'surrounding'],
 };
